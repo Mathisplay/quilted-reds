f8a1557018b8b396087d4f23461157f49cea6d3e
Mathisplay <mateuszkaminski3@o2.pl>
2016-05-11 23:25:10 +0200

    Update input_text.php

--- input_text.php
+++ input_text.php
@@ -7,6 +7,10 @@
 </head>
 <body>
 
+<script>
+    function back(){alert("wow")};
+</script>
+
 Your password is: <?php $text = $_GET["pass"];
 echo $text;
 $correct = "abc";
@@ -16,10 +20,5 @@ echo $isgood;
 if($isgood == 0): ?>
 back();
 <?php endif; ?>
-
-<script>
-    function back(){alert("wow")};
-</script>
-
 </body>
 </html> 
