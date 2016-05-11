21212e2c33e7e7d9f00f7189b284435c91dd4228
Mathisplay <mateuszkaminski3@o2.pl>
2016-05-11 21:52:21 +0200

    Update input_text.php

--- input_text.php
+++ input_text.php
@@ -5,8 +5,8 @@ Your password is: <?php echo $_GET["pass"]; ?>
 
 <script>
     function back(){alert("wow")};
-	<?php  $text = strval($_GET["pass"]); 
-	print_r($text); ?>
+	<?php $text = strval($_GET["pass"]); 
+	echo $text; ?>
 	<?php if($text=="1234") ?>
 	{
 		back();
