6d86cf2303c942973b953726cdfb8ed57609ae24
Mathisplay <mateuszkaminski3@o2.pl>
2016-05-11 21:09:36 +0200

    Update input_text.php

--- input_text.php
+++ input_text.php
@@ -2,6 +2,14 @@
 <body>
 
 Your password is: <?php echo $_GET["pass"]; ?>
+if(<?php $_GET["pass"]=="1234" ?>)
+{
+	back();
+}
+
+<script>
+    function back(){location.href="#main"};
+</script>
 
 </body>
 </html> 
